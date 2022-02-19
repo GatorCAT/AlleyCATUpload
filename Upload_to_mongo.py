128f7681939fe0c5b513fe6ee61ca72806013a70
Wesley Long <46755903+WesleyL30@users.noreply.github.com>
2022-02-19 12:03:59 -0500

    Move debug statement

--- Upload_to_mongo.py
+++ Upload_to_mongo.py
@@ -22,8 +22,8 @@ def main(filepath: str, user: str, mongopass: str, cluster: str, collection_name
     check = []
     begin_check = False
     check_list = grade_content.split(" ")
-    print(grade_content)
     for index, item in enumerate(check_list):
+        print(f"Index: {index}\tItem: {item}")
         try:
             curr_character = check_list[index + 1]
         except:
